58b0ee0ab2bf0ec80bcf4924b4003965debc632b
chino <sarachoesteban@gmail.com>
2022-09-18 02:25:50 -0300

    Se inicia proyecto en Spring

---
+++ bibliog25-api/src/main/java/com/G25/bibliog25/Bibliog25Application.java
@@ -0,0 +1,13 @@
+package com.G25.bibliog25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bibliog25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bibliog25Application.class, args);
+	}
+
+}
